e07b65cd14f095859d8137f0d7540992440e2852
Pr0meth3u5 <54063785+Pr0meth3u5@users.noreply.github.com>
2020-10-18 15:14:31 +0530

    removed credentials

--- Text_editor/settings.py
+++ Text_editor/settings.py
@@ -163,5 +163,5 @@ CKEDITOR_CONFIGS = {
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ssectexteditor@gmail.com'
-EMAIL_HOST_PASSWORD = 'gaganlnmayo'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
